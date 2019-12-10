1da6bb8d739b7521f7c1aac392c4e9c2d2008c54
Chana <chana987@gmail.com>
2019-12-10 18:27:31 +0200

    heroku

--- server/server.js
+++ server/server.js
@@ -11,16 +11,14 @@ mongoose.connect("mongodb://localhost/bankDB", {
 	useUnifiedTopology: true,
 	useFindAndModify: false
 })
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
 
-    next()
-})
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(express.static(path.join(__dirname, 'build')));
 app.use("/", api)
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Running server on port ${port}`))
