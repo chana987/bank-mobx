25e84669ee35718368b99d603ddcb2fc83bbc3b8
Chana <chana987@gmail.com>
2020-01-12 15:34:21 +0200

    heroku fixes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 const app = express()
 const api = require("./routes/api")
-const port = process.env.SERVER_PORT || 4000
+const port = process.env.PORT || 4000
 const bodyParser = require("body-parser")
 
 const mongoose = require("mongoose")
@@ -12,16 +12,13 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bankDB", {
 	useFindAndModify: false
 })
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-    next()
-})
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'build')));
 app.use("/", api)
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Running server on port ${port}`))
